a149c5d14ad8577c1b70567e12ea7915031573ab
2MPP <37121247+2MPP@users.noreply.github.com>
2020-11-19 20:27:34 +0000

    changed test command

--- commands/owner/test.js
+++ commands/owner/test.js
@@ -5,6 +5,6 @@ module.exports = {
 	description: '',
 	usage: '<input>',
 	run: (client, message, args) => {
-		message.channel.send(`What did u think was gonna happen?`)
+		message.channel.send(`What did u think was gonna happen????`)
 	},
 };
