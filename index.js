e38eeae8f179d9908d1ea0f983c554d64df58662
2MPP <37121247+2MPP@users.noreply.github.com>
2020-11-24 18:45:39 +0000

    added stuff

--- index.js
+++ index.js
@@ -70,13 +70,12 @@ client.on('guildCreate', async guild => {
 
 	client.users.fetch('251428574119067648', false).then(user => {
 
-			const uwu = new MessageEmbed()
-				.setTitle('**New server UwU**')
-				.setColor('#FFC0CB')
-				.setDescription(`I was added to **${guild.name}** \n Server count is now ${client.guilds.cache.size} \n Guild id is ${guild.id} \n Server has ${guild.members.cache.size} Members`);
-			user.send(uwu);
+		const uwu = new MessageEmbed()
+			.setTitle('**New server UwU**')
+			.setColor('#FFC0CB')
+			.setDescription(`I was added to **${guild.name}** \n Server count is now ${client.guilds.cache.size} \n Guild id is ${guild.id} \n Server has ${guild.members.cache.size} Members`);
+		user.send(uwu);
 
-		});
 	});
 });
 
