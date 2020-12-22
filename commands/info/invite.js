9810f44e6f6ddb3740433e2c7e72665c45c0b006
Mike <noobinthehouse345@gmail.com>
2020-12-22 21:30:04 +0000

    Invite command is more clear

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -9,7 +9,7 @@ module.exports = {
 	run: async (client, message, args) => {
 		const embed = new Discord.MessageEmbed()
 
-			.setTitle('**Hi**')
+			.setTitle('**Click here to invite!**')
 			.setURL('https://discordapp.com/oauth2/authorize?client_id=778411215470067732&scope=bot&permissions=8')
 			.setDescription('Need help dm me 2MP#6384 or curry#4094')
 			.setColor('RED');
