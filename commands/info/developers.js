004f7bf9c0a3f762bda9e0002311cbdc17a432fd
2MPP <37121247+2MPP@users.noreply.github.com>
2020-12-23 18:11:19 +0000

    auto update dev to show correct info

--- commands/info/developers.js
+++ commands/info/developers.js
@@ -6,13 +6,16 @@ module.exports = {
 	category: 'info',
 	description: 'Shows the owners of poggers bot!',
 	usage: '<input>',
-	run: (client, message) => {
+	run: async (client, message, args) => {
+		const cu = await client.users.fetch('405771597761216522')
+		const mp = await client.users.fetch('251428574119067648')
+
 		const embed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Developers')
             .addFields(
-                {name: 'Owner', value: '2MP#0002'},
-				{name: 'Co-Owner', value: 'curry#6969'},
+                {name: 'Owner', value: mp.tag},
+				{name: 'Co-Owner', value: cu.tag},
 				{name: '\u200B', value: '2MP is the main developer for Poggers Bot, he has done majority of the code. \n Meanwhile, curry is open at all times for support and corrects capital letters because 2MP always forgets.'}
 			)
 
