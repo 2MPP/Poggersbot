9809d5e1455f8138ed33c94b408175d30e93353d
2MPP <37121247+2MPP@users.noreply.github.com>
2021-02-19 10:55:31 +0000

    fix lb

--- commands/economy/leaderboard.js
+++ commands/economy/leaderboard.js
@@ -1,4 +1,4 @@
-const MessageEmbed = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const ecoModdel = require('../../models/eco');
 module.exports = {
 	name: 'leaderboard',
